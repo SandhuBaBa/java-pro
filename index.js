541172a9f0aa4327c10e138b237fcebabcfe865a
SandhuBaBa <144222640+SandhuBaBa@users.noreply.github.com>
2023-09-18 04:01:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "React Js Developer", "Web Developer"],
+    typeSpeed: 30,
+    backSpeed: 60,
+    backDelay: 100,
+    loop: true
+  });
+  
+
+
+
+
+
+
+
+
+  
